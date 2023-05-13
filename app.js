e1d017357cec58aa27337207799f17a3b51d60d8
Zeeshan Saeed <zsaeed830@gmail.com>
2023-05-13 23:32:00 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const users = require("./routes/users");
+const connectDB = require("./db/connect");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.use(express.json());
+
+// Routes
+app.use("/api/v1/users", users);
+
+// Connection with DB
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`Server is running on port ${port}`));
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+start();
---
+++ controllers/users.js
@@ -0,0 +1,13 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const User = require("../models/User");
+
+///////////////////////////////
+//////// Register User
+
+const registerUser = async (req, res) => {};
+
+module.exports = {
+  registerUser,
+};
