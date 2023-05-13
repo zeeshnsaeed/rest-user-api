43f0dd77ebf385c426da8fe6afa922cd594ba48e
Zeeshan Saeed <zsaeed830@gmail.com>
2023-05-13 23:46:18 +0500

    Feat: login user handler completed

--- controllers/users.js
+++ controllers/users.js
@@ -3,8 +3,8 @@ const jwt = require("jsonwebtoken");
 
 const User = require("../models/User");
 
-///////////////////////////////
-//////// Register User
+//////////////////////////////////
+//////// Register User Controller
 
 const registerUser = async (req, res) => {
   const { username, email, password, role } = req.body;
@@ -40,6 +40,41 @@ const registerUser = async (req, res) => {
   }
 };
 
+/////////////////////////////////
+//////// Login User Controller
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  // Check if the email and password are present
+  if (!email || !password) {
+    return res.status(400).json({ error: "Email and password are required" });
+  }
+
+  // Find the user by email
+  const user = await User.findOne({ email });
+  if (!user) {
+    return res.status(401).json({ error: "Invalid email or password" });
+  }
+
+  // Check if the password is correct
+  const passwordMatch = await bcrypt.compare(password, user.password);
+  if (!passwordMatch) {
+    return res.status(401).json({ error: "Invalid email or password" });
+  }
+
+  // If user is valid, create a JWT token and send it in the response
+  const payload = {
+    userId: user.id,
+    role: user.role,
+    timestamp: Date.now(),
+  };
+  const token = jwt.sign(payload, "secret", { expiresIn: "1h" });
+
+  // Send the token as a response
+  res.json({ token });
+};
+
 module.exports = {
   registerUser,
+  loginUser,
 };
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const { registerUser } = require("../controllers/users");
+const { registerUser, loginUser } = require("../controllers/users");
 
 router.post("/register", registerUser);
+router.post("/login", loginUser);
