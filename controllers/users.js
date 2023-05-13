803d7dc67f252bb80250809d53b01e8abd813fcb
Zeeshan Saeed <zsaeed830@gmail.com>
2023-05-13 23:35:15 +0500

    Feat: register new user completed

--- controllers/users.js
+++ controllers/users.js
@@ -6,7 +6,39 @@ const User = require("../models/User");
 ///////////////////////////////
 //////// Register User
 
-const registerUser = async (req, res) => {};
+const registerUser = async (req, res) => {
+  const { username, email, password, role } = req.body;
+
+  // Hash the password
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  // Create a new user
+  const user = new User({
+    username,
+    email,
+    password: hashedPassword,
+    role,
+  });
+
+  try {
+    // Save the user to the database
+    await user.save();
+
+    // If user is valid, create a JWT token and send it in the response
+    const payload = {
+      userId: user.id,
+      role: user.role,
+      timestamp: Date.now(),
+    };
+    const token = jwt.sign(payload, "secret", { expiresIn: "1h" });
+
+    // Send the response with the JWT
+    res.status(201).json({ token });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Server error" });
+  }
+};
 
 module.exports = {
   registerUser,
